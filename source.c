786ed337b93a48f816f227146dba28054897cfca
FilipVlahy <fpfifo40@gmail.com>
2021-06-21 05:25:09 +0200

    Upravene dokoncene

--- source.c
+++ source.c
@@ -3,116 +3,29 @@
 #include <string.h>
 
 unsigned int strana_susediaca_na_zvazku(unsigned int page, unsigned int no_pages, unsigned int bundle_size)
-{    
-    unsigned int one_sheet=bundle_size*4;
-    unsigned int empty_pages_added=0;
-
-    while (no_pages % one_sheet != 0)
-    {
-        no_pages++;
-        empty_pages_added++;
-    }
-    if (empty_pages_added!=0)
-        printf("Total number of pages has been changed.\n%u empty pages have been added to balance your book by bundle_size.\n", empty_pages_added);
-
-    double no_sheets=no_pages/(bundle_size*4);
-    unsigned int sheet[one_sheet];
-
-    
+{
     unsigned int next=0;
-    unsigned int start=1;
-    unsigned int end=bundle_size*4;
+    unsigned int one_sheet=bundle_size*4;
+    unsigned int max_sheets=no_pages/one_sheet+1;
+    unsigned int sheet=((page-1)/one_sheet)+1;
 
+    unsigned int last_sheet_size;
+    unsigned int page_in_sheet;
+    unsigned int next_in_sheet;
 
-    unsigned int book[(int)no_sheets][one_sheet/2][2];
+    if ((((no_pages+1)%one_sheet)/2)*2==0)
+        last_sheet_size=one_sheet;
+    else
+        last_sheet_size=(((no_pages+1)%one_sheet)/2)*2;
 
-    //memset(book,0,sizeof(book));
+    page_in_sheet=((page-1)%one_sheet)+1;
 
-    int i=0;
-
-    for (int j=0;j<one_sheet;j++)
-    {
-        sheet[j]= end;
-        sheet[j+1]= start;
-
-        end--;
-        start++;
-        j++;
-    }
-
-    int p=0;
-
-    for (int j=0;j< one_sheet/2;j++)
-    {
-        book[i][j][0]=sheet[p];
-        book[i][j][1]=sheet[p+1];
-        p+=2;
-    }
-
-    for (int j=0;j< one_sheet;j++)
-    {
-        if (book[i][j][0]==page)
-        {
-            next = book[i][j][1];
-
-            return next;
-            break;
-        }
-        if (book[i][j][1]==page)
-        {
-            next = book[i][j][0];
-
-            return next;
-            break;
-        }
-    }
-
-    start=1;
-    end=one_sheet;
-
-    for (i=1;i<no_sheets;i++)
-    {    
-        start+=one_sheet;
-        end+=one_sheet;
-
-        for (int j=0;j<one_sheet;j++)
-        {
-            sheet[j]= end;
-            sheet[j+1]= start;
-
-            end--;
-            start++;
-            j++;
-        }
-
-        int p=0;
-
-        for (int j=0;j< one_sheet/2;j++)
-        {
-            book[i][j][0]=sheet[p];
-            book[i][j][1]=sheet[p+1];
-            p+=2;
-        }
+    if (sheet==max_sheets)
+        next_in_sheet=last_sheet_size-page_in_sheet+1;
+    else
+        next_in_sheet=one_sheet-page_in_sheet+1;
     
-        for (int j=0;j< one_sheet;j++)
-        {
-            if (book[i][j][0]==page)
-            {
-                next = book[i][j][1];
-
-                return next;
-                break;
-            }
-            if (book[i][j][1]==page)
-            {
-                next = book[i][j][0];
-
-                return next;
-                break;
-            }
-        }
-
-    }
+    next=(sheet-1)*one_sheet+next_in_sheet;
 
     return next;
 }
@@ -121,9 +34,9 @@ int main(void)
 {
     unsigned int page, no_pages, bundle_size;
 
-    page = 1;
-    no_pages = 16;
-    bundle_size = 4;
+    page = 38;
+    no_pages = 48;
+    bundle_size = 20;
 
     printf("Sused pre %u je %u", page,strana_susediaca_na_zvazku(page,no_pages,bundle_size));
 
